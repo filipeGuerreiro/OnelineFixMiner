82a5a2a68686258fdf2c4ee71f381cc1341d1694
filipe <filipe@filipe.nii>
2017-03-10 13:02:04 +0900

    Added remote url and fetch all modifications

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # OnelineFixMiner
-Mine one-line fix commit from git repository
+Mine one-line fix commit from git repository.
+Modified to fetch repository automatically from git url.
+In addition, if no url is provided, fetches all the repositories from wordpress plugins.
 
 ## Install
 You need [GitPython](https://github.com/gitpython-developers/GitPython).
@@ -9,5 +11,5 @@ $ sudo pip install gitpython
 ```
 ## Usage
 ```
-$ ./mine.py <repository directory (default:.)> <branch name (default:master)>
+$ ./mine.py <repository remote url (default:fetch all repositories from wordpress plugins)> <branch name (default:master)>
 ```
--- mine.py
+++ mine.py
@@ -1,6 +1,14 @@
 #!/usr/bin/env python
 import git
 import sys
+import os
+from pygithub3 import Github
+import getpass
+import requests
+from requests.auth import HTTPBasicAuth
+import shutil
+
+DIR_NAME = "fetched_repos/"
 
 def is_oneline_fix(commit):
     if len(commit.parents) != 1:
@@ -11,18 +19,63 @@ def is_oneline_fix(commit):
 
 def diff_parent(commit):
     parent = commit.parents[0]
-    print parent.diff(commit, create_patch=True)[0].diff
+    diff = parent.diff(commit, create_patch=True)[0]
+    #print dir(diff)
+    return diff.diff
 
-def main(argv):
-    path = argv[1] if len(argv) > 1 else "."
+def get_all_repos(user, password):
+    repo_urls = []
+    page = 1
+    while True:
+        url = 'https://api.github.com/orgs/wp-plugins/repos?per_page=100&page={0}'
+        r = requests.get(url.format(page), auth = HTTPBasicAuth(user, password))
+        if r.status_code == 200:
+            rdata = r.json()
+            for repo in rdata:
+                repo_urls.append(repo['clone_url'])
+            if (len(rdata) >= 100):
+                page = page + 1
+		print('{0} repos so far'.format(len(repo_urls)))
+            else:
+                print('Found {0} repos.'.format(len(repo_urls)))
+                break
+        else:
+            print(r)
+            return repo_urls
+    return repo_urls
+
+def main(argv):    
+    inputUrl = argv[1] if len(argv) > 1 else ""
     branch = argv[2] if len(argv) > 2 else "master"
-    repo = git.Repo(path)
-    for c in repo.iter_commits(branch):
-        if is_oneline_fix(c):
-            print "=" * 48
-            print "Commit: " + c.hexsha
-            print "Summary: " + c.summary
-            print diff_parent(c)
+
+    if not inputUrl:
+        print "Fetching all repositories from wp-plugins" 
+        user = raw_input('Insert your github username: ')
+        password = getpass.getpass("Insert your password: ")
+        repo_urls = get_all_repos(user, password)
+    else:
+        repo_urls = [inputUrl]
+
+    for remoteUrl in repo_urls:
+        name = remoteUrl.split("/")[-1]
+        if not os.path.exists(DIR_NAME):
+            os.mkdir(DIR_NAME)   
+        if os.path.exists(DIR_NAME + name):
+            shutil.rmtree(DIR_NAME + name)
+            os.mkdir(DIR_NAME + name)
+        repo = git.Repo.init(DIR_NAME + name)
+        origin = repo.create_remote('origin', remoteUrl)
+        origin.fetch()
+        origin.pull(origin.refs[0].remote_head)
+
+        for c in repo.iter_commits(branch):
+            if is_oneline_fix(c):
+                print "=" * 48
+                print "Commit: " + c.hexsha
+                print "Summary: " + c.summary
+                diff = diff_parent(c)
+                with open(DIR_NAME + name +".out", "a") as output:
+                    output.write(diff)
 
 if __name__ == '__main__':
     main(sys.argv)
